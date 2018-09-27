7c6e3fd78e4f50d19b4b317e6700fc0f26c4d33a
Nonpawit Teerachetmongkol <nonpawit.tee@gmail.com>
2018-09-27 22:25:46 +0700

    Update project setting

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup, find_packages
 
 install_requires = [
-        "Scrapy>=0.24.4",
+        "Scrapy>=1.3.0",
         "Flask>=0.10.1",
         "Twisted>=15.4.0",
         "six>=1.10.0"
 ]
 
 setup(
-    name='Arachne',
+    name='ArachneScrapy',
     version='0.5.1',
-    author='Kiran Koduru',
-    author_email='kiranrkoduru@gmail.com',
+    author='Nonpawit Teerachetmongkol',
+    author_email='nonpawit.tee@gmail.com',
     packages=find_packages(),
     test_suite='arachne.tests',
-    url='https://github.com/kirankoduru/arachne',
+    url='https://github.com/n3n/arachne',
     license='BSD',
     description='API for Scrapy spiders',
     long_description=open('README.rst').read(),
